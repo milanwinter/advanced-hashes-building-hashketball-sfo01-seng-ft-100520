b2d29969c3110722e0d0432cbfd92589368d2703
Milan Winter <milanwinter8@gmail.com>
2020-09-25 21:09:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,13 +5,23 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black","White"],
       :players =>[
+        {:player_name => "", :number => "", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""},
+        {:player_name => "", :number => "", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""},
+        {:player_name => "", :number => "", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""},
+        {:player_name => "", :number => "", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""},
         {:player_name => "", :number => "", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""}
       ]
     },
     :away => {
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise","Purple"],
-      :players => []
+      :players => [
+        {:player_name => "", :number => "", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""},
+        {:player_name => "", :number => "", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""},
+        {:player_name => "", :number => "", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""},
+        {:player_name => "", :number => "", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""},
+        {:player_name => "", :number => "", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""}
+      ]
     }
   }
 end
